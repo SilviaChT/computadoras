a2dbc284fd92e7261b5a4672fb2a38adde3ee3e3
SilviaChT <grtoti2015@gmail.com>
2023-06-18 11:16:27 -0500

    subiendo proyecto

---
+++ js/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('https://elliotgaramendi.github.io/web-development-tecsup/g21/semana7/alumnos-felices/json/students.json')
+    .then(response => response.json())
+    .then(data => {
+      const studentCards = document.getElementById('studentCards');
+      data.forEach((element, index) => {
+        const {categoria, subcategoria,imagen, marca, procesador, memoria, discoduro, color, precio, otros} = element;
+        studentCards.innerHTML += `
+          <div class="card" key=${index}>
+            <div class="card__body">
+              <img src="${image || 'https://emojitool.com/img/whatsapp/2.20.198.15/boar-1943.png'}" alt="${name}" width="128"
+                height="128" class="card__img" />
+              <h2 class="card__title">
+                ${categoria}
+                <sup>${subcategoria}</sup>
+              </h2>
+              <p class="card__text">
+                ${marca}
+              </p>
+              <ul class="card__text">
+                ${procesador}
+              </ul>
+              <ul class="card__text">
+                ${memoria}
+              </ul>
+              <ul class="card__text">
+                ${discoduro}
+              </ul>
+              <ul class="card__text">
+                ${color}
+              </ul>
+              <ul class="card__text">
+                ${precio}
+              </ul>
+              <ul class="card__text">
+                ${otros}
+              </ul>
+            </div>
+          </div>
+        `;
+      });
+    });
+});
